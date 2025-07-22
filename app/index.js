c3e8b161f060063679f32a712e9b9acecd0145eb
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-07-22 11:34:34 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 80;
 
+// Serve index.html when visiting "/"
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js app running on custom AMI! by using packer');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Start server
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
