55a6db7386076c6adc06a0272ba035bf0bee7bf7
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-07-22 12:04:54 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -4,11 +4,11 @@ const app = express();
 const port = 80;
 
 // Serve static files from "art" folder
-app.use(express.static(path.join(__dirname, 'art')));
+app.use(express.static(path.join(__dirname, 'Netflix')));
 
 // Optional: redirect "/" to index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'art', 'index.html'));
+  res.sendFile(path.join(__dirname, 'Netflix', 'index.html'));
 });
 
 app.listen(port, () => {
