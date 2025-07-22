34cc9b370cbeeefabf36424a6cec986c757109df
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-07-22 12:02:54 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,12 +3,14 @@ const path = require('path');
 const app = express();
 const port = 80;
 
-// Serve index.html when visiting "/"
+// Serve static files from "art" folder
+app.use(express.static(path.join(__dirname, 'art')));
+
+// Optional: redirect "/" to index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'art', 'index.html'));
 });
 
-// Start server
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
